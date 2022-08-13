8607ee96060457f09433af1cddca30f78ef2d3b4
Lucas Córdoba <lcordobadev@gmail.com>
2022-08-13 16:29:51 -0300

    HS-00 First commit

---
+++ src/main/java/com/lucas/hardwarestore/HardwareStoreApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.hardwarestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardwareStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HardwareStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lucas/hardwarestore/HardwareStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.hardwarestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardwareStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
